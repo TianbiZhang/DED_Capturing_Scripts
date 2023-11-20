10a1396bb8127b47f78eac411f7cb34178d5d126
TianbiZhang <zhang619@student.ubc.ca>
2023-11-20 10:58:03 -0800

    First release

--- scripts/device_readout_parameters.py
+++
@@ -1,8 +0,0 @@
-dev = pixet.devices()[0]
-params = dev.parameters()
-par = params.get("TriggerStg")
-#par.setBOOL(True)
-#par.setDouble(10)
-#par.setByte(0)
-
-
--- scripts/install_python_module.py
+++
@@ -1,3 +0,0 @@
-import pip._internal
-module_name_to_install = "numpy"
-pip._internal.main(["install", "--target=libs", module_name_to_install])
